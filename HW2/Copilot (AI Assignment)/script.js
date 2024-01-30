a961900a11ecf5829c9cdc7c51f54dd422b637bb
PatrickReiman <patrickreiman122@gmail.com>
2024-01-29 23:43:12 -0700

    HW 2
    This contains all files relevant to Homework Assignment 2, including the AI portion.

---
+++ HW2/Copilot (AI Assignment)/script.js
@@ -0,0 +1,14 @@
+console.log("Favorite Websites:");
+console.log("1. www.github.org");
+console.log("2. www.youtube.com");
+console.log("3. www.wikipedia.com");
+
+console.log("\nFavorite Games:");
+console.log("1. Chess - A strategy game played by two opponents on a checkered board with specially designed pieces of contrasting colours, commonly white and black. The objective of the game is to capture the opponent's king..");
+console.log("2. Battleship - A strategy type guessing game for two players. It is played on ruled grids (paper or board) on which each player's fleet of warships are marked. The locations of the fleets are concealed from the other player.");
+console.log("3. Uno - A multi-player card game in which the objective is to be the first player to get rid of all the cards in their hand.");
+
+console.log("\nFavorite Artists:");
+console.log("1. Pablo Picasso - Pablo Picasso was a Spanish painter, sculptor, printmaker, ceramicist, and theatre designer who spent most of his adult life in France. He is considered one of the greatest artists of the 20th century and is known for co-founding the Cubist movement, the invention of constructed sculpture, the co-invention of collage, and for the wide variety of styles that he helped develop and explore.");
+console.log("2. Michelangelo di Lodovico Buonarroti Simoni - An Italian sculptor, painter, architect, and poet of the High Renaissance. He was born on March 6, 1475 in Caprese, Republic of Florence and died on February 18, 1564 in Rome, Papal States. Michelangelo’s work was inspired by models from classical antiquity and had a lasting influence on Western art.");
+console.log("3. Donatello - an Italian sculptor of the Renaissance period. He was born in Florence around 1386 and died on December 13, 1466, in the same city. Donatello was one of the greatest Florentine sculptors and a leading figure of the early Renaissance. He was extremely versatile, working in stone, metal, wood, terracotta, and bronze.");
---
+++ HW2/script.js
@@ -0,0 +1,15 @@
+function favWeb(){
+    console.log("My favorite websites are youtube.com, instagram.com, and twitter.com.");
+}
+
+function favGame(){
+    console.log("My favorite games are Minecraft, Terraria, and Civ5.")
+}
+
+function favArtist(){
+    console.log("My favorite artists (artist being anyone who creates something) are Kyle Hill, Kevin Penkin, and Jean Pierre Ugarte. Kyle Hill is a science educator on YouTube who covers a variety of topics from silly comic/cartoon theories to real-world [nuclear] disasters and the safeness of nuclear power. Kevin Penkin is a music artist who primarily makes soundtracks, I particularly like how he incorporates such unique instruments and vocalizations into his music rather than the standard we see now-a-days. Jean-Pierre Ugarte is an artist who specializes in painting large concrete architecture that almost seemlessly incorporates into nature. It borders on brutalist art, however, the image seems surreal as the paintings obviously depict a non-manmade structure.")
+}
+
+function changeBackground(){
+    document.body.style.backgroundSize = "0";
+}
