1cb78fd003892bc344b0ef58d8f8da4f95b38caf
PatrickReiman <patrickreiman122@gmail.com>
2024-02-06 02:07:09 -0700

    1st Patch
    Got working array of functions

--- HW3/script.js
+++ HW3/script.js
@@ -1,55 +1,108 @@
-var y = 0
-var progress = 0;
+var stageProgression = 0;
+var aisles = [buyCheese, buyIcecream, buyApples]
 
 function buttonChecker(x){
-    progress = x;
-    console.log(progress);
-    if (x == 1 || y == 0){
-        progress += 9;
-        buyCheese();
-        y = 0;
-    } else if (x == 2 || y == 1){
-        progress += 18;
-        buyWatermelon();
-        y = 1;
-    } else {
-        buyApples();
-        y = 2;
-        progress += 100;
+    if (1 > stageProgression){
+        aisles[x](x);
+        console.log ("it shouldn't be doing this anymore")
     }
+
+
+
+
+    /* if (x == 1){
+        if (stageProgression == 0){
+        buyCheese();
+        } else {
+            cheeseSelect(x);
+        }
+    } else if (x == 2){
+        buyIcecream();
+    } else if (x == 3){
+
+        buyApples();
+    } else {
+        console.log("fail");
+    } */
+    stageProgression += 1;
 }
 
 function buyCheese(){
+    console.log("yes1");
     document.body.style.backgroundImage = "url('./img/cheese.jpg')";
     document.getElementById("button1").innerHTML = "Feta";
     document.getElementById("button2").innerHTML = "Brie";
     document.getElementById("button3").innerHTML = "Mozzarella";
     document.getElementById("h1").innerHTML = "You walk into the cheese aisle and see an assortment of cheese";
     document.getElementById("h2").innerHTML = "Which do you choose?";
-    console.log(progress);
+}
 
-    if (10 >= progress >= 3){
+function cheeseSelect(x){
+    if (x == 1){
         document.body.style.backgroundImage = "url('./img/feta.jpg')";
         document.getElementById("h1").innerHTML = "You walk out of the store with your feta cheese!";
         document.getElementById("h2").innerHTML = "";
-    } else if (20 >= progress >= 13){
+        document.getElementsByClassName("button")[0].style.visibility = "hidden";
+        document.getElementsByClassName("button")[1].style.visibility = "hidden";
+        document.getElementsByClassName("button")[2].style.visibility = "hidden";
+    } else if (x == 2){
         document.body.style.backgroundImage = "url('./img/brie.webp')";
         document.getElementById("h1").innerHTML = "You walk out of the store with your brie cheese!";
         document.getElementById("h2").innerHTML = "";
-    } else {
+        document.getElementsByClassName("button")[0].style.visibility = "hidden";
+        document.getElementsByClassName("button")[1].style.visibility = "hidden";
+        document.getElementsByClassName("button")[2].style.visibility = "hidden";
+    } else if (x == 3) {
         document.body.style.backgroundImage = "url('./img/mozzarella.jpg')";
         document.getElementById("h1").innerHTML = "You walk out of the store with your mozzarella cheese!";
         document.getElementById("h2").innerHTML = "";
+        document.getElementsByClassName("button")[0].style.visibility = "hidden";
+        document.getElementsByClassName("button")[1].style.visibility = "hidden";
+        document.getElementsByClassName("button")[2].style.visibility = "hidden";
+    } else {
+        console.log("failure");
     }
-    console.log(progress);
-
 }
 
-function buyWatermelon(){
-    document.body.style.backgroundImage = "url('./img/watermelon.jpg')";
+function buyIcecream(){
+    console.log("yes2");
+    document.body.style.backgroundImage = "url('./img/icecream.webp')";
+    document.getElementById("button1").innerHTML = "Vanilla";
+    document.getElementById("button2").innerHTML = "Chocolate";
+    document.getElementById("button3").innerHTML = "Mint";
+    document.getElementById("h1").innerHTML = "You walk into the ice cream aisle and see an assortment of ice cream";
+    document.getElementById("h2").innerHTML = "Which do you choose?";
+}
+
+function icecreamSelect(){
+    if (count == 3){
+        document.body.style.backgroundImage = "url('./img/vanilla.jpg')";
+        document.getElementById("h1").innerHTML = "You walk out of the store with your vanilla ice cream!";
+        document.getElementById("h2").innerHTML = "";
+        document.getElementsByClassName("button")[0].style.visibility = "hidden";
+        document.getElementsByClassName("button")[1].style.visibility = "hidden";
+        document.getElementsByClassName("button")[2].style.visibility = "hidden";
+    } else if (count == 4){
+        document.body.style.backgroundImage = "url('./img/chocolate.jpg')";
+        document.getElementById("h1").innerHTML = "You walk out of the store with your chocolate ice cream!";
+        document.getElementById("h2").innerHTML = "";
+        document.getElementsByClassName("button")[0].style.visibility = "hidden";
+        document.getElementsByClassName("button")[1].style.visibility = "hidden";
+        document.getElementsByClassName("button")[2].style.visibility = "hidden";
+    } else if (count == 5) {
+        document.body.style.backgroundImage = "url('./img/mint.jpg')";
+        document.getElementById("h1").innerHTML = "You walk out of the store with your mint ice cream!";
+        document.getElementById("h2").innerHTML = "";
+        document.getElementsByClassName("button")[0].style.visibility = "hidden";
+        document.getElementsByClassName("button")[1].style.visibility = "hidden";
+        document.getElementsByClassName("button")[2].style.visibility = "hidden";
+    } else {
+        console.log("failure");
+    }
 }
 
 function buyApples(){
+    console.log("yes3");
     document.body.style.backgroundImage = "url('./img/apples.jpg')";
 
 }
