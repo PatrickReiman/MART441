5113b21b7e69edcb316d9e4c31a57aed7db17398
PatrickReiman <patrickreiman122@gmail.com>
2024-02-13 11:55:49 -0700

    Patch 2
    Added almost complete functionality to story

--- For Testing/Ex2/script.js
+++ For Testing/Ex2/script.js
@@ -1,3 +1,6 @@
 for(var i = 0; i < 5; i+=2){
     console.log(i);
 }
+
+
+console.log(5/2);
--- HW4/script.js
+++ HW4/script.js
@@ -1,16 +1,17 @@
-var stageProgression = 0;
-var whichAisle = 0;
+var failProgression = 0;
+var progression = 0;
 
 function buttonChecker(x){
-    if(x == -1){
-        return x == -1 ? deskDuty() :
-        x == -3 ? fired() :
-        disasterStrikes();
+    if(x%5 == 0){
+        failProgression++;
+        return failProgression == 1 ? deskDuty() :
+        fired();
     } else {
-        return x == 1 ? launch() :
-        x == 2 ? noTurningBack() :
-        x == 3 ? couplingSuccess() :
-        x == 4 ? repair() :
+        progression++;
+        return progression == 1 ? launch() :
+        progression == 2 ? inSpace() :
+        progression == 3 ? approachSatellite() :
+        progression == 4 ? repair() :
         returnToEarth();
     }
 }
@@ -37,7 +38,62 @@ function launch(){
     document.body.style.backgroundImage = "url('./img/rocketlaunch.jpg')";
     document.getElementById("h1").innerHTML = "You accept your mission and launch with your crew into space.";
     document.getElementById("h2").innerHTML = "";
-    document.getElementById("button1").innerHTML = "Onward"
-    document.getElementById("button0").style.visibility = "hidden";
+    document.getElementById("button0").innerHTML = "Onward"
+    document.getElementById("button1").style.visibility = "hidden";
+    document.getElementById("button0").style.marginLeft = '6.5em';
+}
+
+function inSpace(){
+    document.body.style.backgroundImage = "url('./img/inspace.webp')";
+    document.getElementById("h1").innerHTML = "You make it into space and now wonder if you really should attempt your mission.";
+    document.getElementById("h2").innerHTML = "Do you try?";
+    document.getElementById("button1").style.visibility = "visible";
+    document.getElementById("button0").style.marginLeft = '0em';
+    document.getElementById("button0").innerHTML = "Approach Satellite"
+    document.getElementById("button1").innerHTML = "Return to Earth"
+    failProgression++;
+}
+
+function fired(){
+    document.body.style.backgroundImage = "url('./img/fired.webp')";
+    document.getElementById("h1").innerHTML = "You abandon your mission and head back to Earth.";
+    document.getElementById("h2").innerHTML = "NASA promptly fires you for abandoning your mission";
+    var audio = new Audio('./audio/citysound.mp3');
+    audio.play();
+    restartFormatting();
+}
+
+function approachSatellite(){
+    let question = '';
+    document.body.style.backgroundImage = "url('./img/approachsat.jpg')";
+    document.getElementById("h1").innerHTML = "You approach the satellite in the space shuttle and it finally comes into view.";
+    document.getElementById("h2").innerHTML = "You must now couple the satellite to the shuttle";
+    for (let i = 0; i < 2; i++){
+        document.getElementsByClassName("button")[i].style.visibility = "hidden";
+    }
+    document.body.style.backgroundPosition = "left bottom";
+    setTimeout(() => { question = prompt("How many feet away is the satellite for the shuttle?", ""); }, 500);
+    setTimeout(() => { successOrFailure(question); }, 550);
+}
+
+function successOrFailure(x){
+    if(x >= 1 && x <= 250){
+        document.body.style.backgroundImage = "url('./img/successfuldocking.jpg')";
+        document.getElementById("h1").innerHTML = "You successfully couple the satellite to the space shuttle.";
+        document.getElementById("h2").innerHTML = "Now you must make the emergency repairs";
+        document.getElementById("button0").innerHTML = "Attempt Repairs"
+        document.getElementById("button0").style.visibility = "visible";
+        document.getElementById("button0").style.marginLeft = '6.5em';
+    } else {
+        document.body.style.backgroundImage = "url('./img/disasterstrikes.jpg')";
+        document.getElementById("h1").innerHTML = "You misjudge the distance to the satellite and accidently crash into it";
+        document.getElementById("h2").innerHTML = "The space shuttle breaks up and falls back down to Earth";
+        var audio = new Audio('./audio/taps.mp3');
+        audio.play();
+        restartFormatting();
+    }
+}
+
+function repair(){
 
 }
