24981c1cc95a9eb609f3d55fe74acc8ff65a19b8
PatrickReiman <patrickreiman122@gmail.com>
2024-02-22 00:29:49 -0700

    Working Patch
    Mostly working, need to fix issue with image reset.

--- HW5/script.js
+++ HW5/script.js
@@ -1,9 +1,37 @@
-var failProgression = 0;
-var progression = 0;
-var success = Math.floor(Math.random() * 101);
+const arraySize = 12
+const numberArray = [];
+const tempArray = ['', ''];
+var reactivateButton = 0;
+var viewableImages = 0;
 
 function buttonChecker(x){
-    setTimeout(() => { console.log("worked"); }, 3000);
+    tempArray.splice(viewableImages, 1, "i"+Math.round([numberArray[x-1]]/2)+".jpg");
+    viewableImages++;
+    document.getElementById("button"+x).disabled = true;
+    if (viewableImages == 2){
+        document.getElementById("button"+x).style.background = "url('./img/i"+Math.round([numberArray[x-1]]/2)+".jpg')";
+        console.log("inner: "+tempArray)
+        if(tempArray[0] == tempArray[1]){
+            var audio = new Audio('./audio/correct.mp3');
+            audio.play();
+            document.addEventListener("DOMContentLoaded", function(event) {
+                document.getElementById("button"+tempArray[0]).disabled = true;
+                document.getElementById("button"+tempArray[1]).disabled = true;
+            });
+        } else {
+            var audio = new Audio('./audio/wrong.mp3');
+            audio.play();
+            document.getElementById("button"+x).style.background = "";
+            document.getElementById("button"+reactivateButton).disabled = false;
+            document.getElementById("button"+x).disabled = false;
+        }
+        viewableImages = 0;
+    } else {
+        document.getElementById("button"+x).style.background = "url('./img/i"+Math.round([numberArray[x-1]]/2)+".jpg')";
+    }
+    reactivateButton = x;
+    console.log(tempArray)
+
 }
 
 function resetPage(){
@@ -17,3 +45,19 @@ function restartFormatting(){
     document.getElementById("buttonRestart").style.visibility = "visible";
 }
 
+function disableButton(){
+     document.getElementsByClassName("button").disabled = true;
+     setTimeout(function(){
+       document.getElementsByClassName("button").disabled = false;
+     }, 5000);
+}
+
+function randomSort(){
+    do {
+        const randomNumber = Math
+            .floor(Math.random() * 12) + 1
+        if (!numberArray.includes(randomNumber)) {
+            numberArray.push(randomNumber);
+        }
+    } while (numberArray.length < arraySize);
+}
