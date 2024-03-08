1419ea1a9e5d2e0dc6e70f7a3d5e2d7cdc739a57
PatrickReiman <patrickreiman122@gmail.com>
2024-03-08 01:52:53 -0700

    HW7
    Pretty much full build

--- HW7/index.js
+++ HW7/index.js
@@ -1,21 +1,40 @@
-var jsonObject = {};
-var userOrder = 0;
+const numberArray = [];
 
-function submitInfo(){
-    jsonObject["user"+userOrder] = {};
-    const firstNameInput = document.getElementById("firstNameInput");
-    jsonObject["user"+userOrder].firstName = firstNameInput.value;
-    const lastNameInput = document.getElementById("lastNameInput");
-    jsonObject["user"+userOrder].lastName = lastNameInput.value;
-    const ageInput = document.getElementById("ageInput");
-    jsonObject["user"+userOrder].age = ageInput.value;
-
-    if (ageInput.value != "" && lastNameInput.value != "" && firstNameInput.value != ""){
-        localStorage.setItem("jsonObject", JSON.stringify(jsonObject));
-        window.location.href = "./pages/match.html";
-        userOrder++;
-        return false;
+class uselessClass {
+    constructor(title, image, description, author, imageYear){
+        this.title = title;
+        this.image = image;
+        this.description = description;
+        this.author = author;
+        this.imageYear = imageYear;
     }
+}
 
-    //optimize with preventDefault()? how?
+const object1 = new uselessClass("AUF1 cannon in Lebanon", "./image/AUF1.jpg", "A UN AMX AuF1 self-propelled artillery piece in Lebanon in 2016", "Fantassin 72", "October 17, 2006");
+const object2 = new uselessClass("APC passes Presidency building in Sarajevo", "./image/APC.jpg", "A United Nations Protection Force (UNPROFOR) armoured personnel carrier passes the Presidency building in Sarajevo in the winter of 1992-1993", "Christian Marechal", "March 1993");
+const object3 = new uselessClass("Ukrainian helicopter during a special operation accompany MONUSCO aviation", "./image/HELICOPTER.jpg", "Ituri Province, DR Congo: Ukrainian helicopter during a special operation accompany MONUSCO aviation", "Nazar Voloshyn", "Earlier than 2016, unable to find exact date")
+
+const objectArray = [object1, object2, object3]
+
+
+function buttonChecker(){
+    if (numberArray == 0 || numberArray == undefined){
+        randomSort();
+    }
+    document.getElementById("title").innerHTML = objectArray[numberArray[0]].title;
+    document.getElementById("image").style.visibility = "visible";
+    document.getElementById("image").src = objectArray[numberArray[0]].image;
+    document.getElementById("description").innerHTML = objectArray[numberArray[0]].description;
+    document.getElementById("author").innerHTML = "Author: " + objectArray[numberArray[0]].author;
+    document.getElementById("imageYear").innerHTML = "Image Year: " + objectArray[numberArray[0]].imageYear;
+    numberArray.splice(0, 1);
+}
+
+function randomSort(){
+    do {
+        const randomNumber = Math.floor(Math.random() * 3)
+        if (!numberArray.includes(randomNumber)) {
+            numberArray.push(randomNumber);
+        }
+    } while (numberArray.length < 3);
 }
