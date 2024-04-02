e04973aff2aeebb36ec81086b652df9da389746c
PatrickReiman <patrickreiman122@gmail.com>
2024-04-02 03:43:23 -0600

    Patch 1

---
+++ For Testing/Ex8/js.js
@@ -0,0 +1,8 @@
+
+function x(){
+const x = document.getElementById("squareTemp");
+const ctx = x.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(100, 100, 50, 50);
+}
--- HW10/index.js
+++ HW10/index.js
@@ -1,48 +1,67 @@
-let currentLat = 0;
-let tempLat = 0;
-let tempClosestCityNumber = 0;
-const tempLatLong = [];
-let bestDistance = 100;
-let target = {};
-let value = {};
-var count = 0;
-
-$(function () {
-    $('#button').on("click", function () {
-        $.getJSON("https://api.wheretheiss.at/v1/satellites/25544", function (data) {
-            document.getElementById("lat").innerHTML = "Latitude: " + data.latitude.toFixed(4);
-            document.getElementById("long").innerHTML = "Longitude: " + data.longitude.toFixed(4);
-            target = data;
-            findClosest(value, target);
-            //goes second
-        });
-        $.getJSON("https://raw.githubusercontent.com/PatrickReiman/MART441/main/HW9/cities.json", function (values) {
-            value = values;
-            //goes first
-        });
-    });
-});
-
-//x2 and y2 are for iss cords
-//x: latitude y: longitude
-
-function findClosest(value, target){
-    console.log(value[1].Latitude); //error checkers that tell me if a failure occurs before the loop
-    console.log(target.latitude);
-
-    for (var i = 0; i < value.length; i ++) {
-        if (bestDistance > Math.sqrt((target.latitude - value[i].Latitude)^2 + (target.longitude - value[i].Longitude)^2)){
-            tempClosestCityNumber = i;
-            bestDistance = Math.sqrt((target.latitude - value[i].Latitude)^2 + (target.longitude - value[i].Longitude)^2);
-            console.log(bestDistance); //just cool to see that it only takes 10 switches to narrow down over 47k cities across the world
-        } else {
-            count++;
-        }
+class superSqaure {
+    constructor(xCord, yCord, scaling, color){
+        this.xCord = xCord;
+        this.yCord = yCord;
+        this.scaling = scaling;
+        this.color = color;
     }
-    console.log("count: " + count); //lets me know if it skipped some cities for some reason
-    document.getElementById("closestCity").innerHTML = "Closest [Major] City: " + value[tempClosestCityNumber].City + ", " + value[tempClosestCityNumber].Country;
-    count = 0;
-    bestDistance = 100;
-    tempClosestCityNumber = 0;
+
+    changexCord(xCord) {
+        this.xCord = xCord;
+    }
+
+    changeyCord(yCord) {
+        this.yCord = yCord;
+    }
+
+    changeScaling(scaling) {
+        this.scaling = scaling;
+    }
+
+    changeColor(color) {
+        this.color = color;
+    }
+
+    get currentxCord() {
+        return this.xCord;
+    }
+
+    get currentyCord() {
+        return this.yCord;
+    }
+
+    get currentScaling() {
+        return this.scaling;
+    }
+
+    get currentColor() {
+        return this.color;
+    }
 }
 
+function randomColor(){
+    color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    console.log(color);
+}
+
+function makeSquare() {
+    const x = document.getElementById("squareTemp");
+    const ctx = x.getContext("2d");
+    playerSquare = new superSqaure(window.innerWidth/2, window.innerHeight/2, 100, color);
+    playerSquare.changeColor(color);
+    ctx.fillStyle = "red";
+    ctx.fillRect(200, 200, 50, 50);
+}
+
+function test(event) {
+    //this is the one line of code I've seen an AI steal (unfortunately I could not find from where) that worked better than any human could suggest, so many people trying to over-engineer a simple random number generator when this works fine
+    //Actually interesting how well this works, picks random number from 0 to 16,777,215, which is how many different colors there are in base 16 when converted to base 10. Then it uses toString(16) to convert the base 10 number into a base 16 value, allowing it to be used to find a random color
+    color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    console.log(event.key);
+
+}
+
+function squares() {
+    //x , y, width, height
+    ctx.fillStyle = playerSquare.currentColor;
+}
