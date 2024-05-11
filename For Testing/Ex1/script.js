dbe14a3de218c4ae955702aaad2b1826a3fc428c
Patrick Reiman <patrickreiman122@gmail.com>
2024-05-10 22:34:30 -0700

    final patch push

--- For Testing/Ex1/script.js
+++ For Testing/Ex1/script.js
@@ -1,7 +1,3 @@
-const array = ['i7.jpg'];
-
-function buttonChecker(x) {
-    if (!numberArray.includes(randomNumber)) {
-        numberArray.push(randomNumber);
-    }
+function test() {
+    console.log(Math.floor(1.6));
 }
